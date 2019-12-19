6c4c9ad255a7984cc2d60d4243451713941bb33c
Maximilian Fricke <mfricke2808@gmail.com>
2019-12-19 13:43:00 +0100

    Add tests for all days

--- src/main.rs
+++ src/main.rs
@@ -97,81 +97,233 @@ fn main() -> Result<()> {
         1 => {
             println!("Part 1: {}", time(day01::part1, input.trim())?);
             println!("Part 2: {}", time(day01::part2, input.trim())?);
-        },
+        }
         2 => {
             println!("Part 1: {}", time(day02::part1, input.trim())?);
             println!("Part 2: {}", time(day02::part2, input.trim())?);
-        },
+        }
         3 => {
             println!("Part 1: {}", time(day03::part1, input.trim())?);
             println!("Part 2: {}", time(day03::part2, input.trim())?);
-        },
+        }
         4 => {
             println!("Part 1: {}", time(day04::part1, input.trim())?);
             println!("Part 2: {}", time(day04::part2, input.trim())?);
-        },
+        }
         5 => {
             println!("Part 1: {}", time(day05::part1, input.trim())?);
             println!("Part 2: {}", time(day05::part2, input.trim())?);
-        },
+        }
         6 => {
             println!("Part 1: {}", time(day06::part1, input.trim())?);
             println!("Part 2: {}", time(day06::part2, input.trim())?);
-        },
+        }
         7 => {
             println!("Part 1: {}", time(day07::part1, input.trim())?);
             println!("Part 2: {}", time(day07::part2, input.trim())?);
-        },
+        }
         8 => {
             println!("Part 1: {}", time(day08::part1, input.trim())?);
             println!("Part 2: {}", time(day08::part2, input.trim())?);
-        },
+        }
         9 => {
             println!("Part 1: {}", time(day09::part1, input.trim())?);
             println!("Part 2: {}", time(day09::part2, input.trim())?);
-        },
+        }
         10 => {
             println!("Part 1: {}", time(day10::part1, input.trim())?);
             println!("Part 2: {}", time(day10::part2, input.trim())?);
-        },
+        }
         11 => {
             println!("Part 1: {}", time(day11::part1, input.trim())?);
             println!("Part 2: {}", time(day11::part2, input.trim())?);
-        },
+        }
         12 => {
             println!("Part 1: {}", time(day12::part1, input.trim())?);
             println!("Part 2: {}", time(day12::part2, input.trim())?);
-        },
+        }
         13 => {
             println!("Part 1: {}", time(day13::part1, input.trim())?);
             println!("Part 2: {}", time(day13::part2, input.trim())?);
-        },
+        }
         14 => {
             println!("Part 1: {}", time(day14::part1, input.trim())?);
             println!("Part 2: {}", time(day14::part2, input.trim())?);
-        },
+        }
         15 => {
             println!("Part 1: {}", time(day15::part1, input.trim())?);
             println!("Part 2: {}", time(day15::part2, input.trim())?);
-        },
+        }
         16 => {
             println!("Part 1: {}", time(day16::part1, input.trim())?);
             println!("Part 2: {}", time(day16::part2, input.trim())?);
-        },
+        }
         17 => {
             println!("Part 1: {}", time(day17::part1, input.trim())?);
             println!("Part 2: {}", time(day17::part2, input.trim())?);
-        },
+        }
         18 => {
             println!("Part 1: {}", time(day18::part1, input.trim())?);
             println!("Part 2: {}", time(day18::part2, input.trim())?);
-        },
+        }
         19 => {
             println!("Part 1: {}", time(day19::part1, input.trim())?);
             println!("Part 2: {}", time(day19::part2, input.trim())?);
-        },
+        }
         _ => unimplemented!(),
     }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn day1() {
+        let inp = include_str!("../input/day01");
+        assert_eq!(crate::day01::part1(inp.trim()).unwrap(), 3479429);
+        assert_eq!(crate::day01::part2(inp.trim()).unwrap(), 5216273);
+    }
+
+    #[test]
+    fn day2() {
+        let inp = include_str!("../input/day02");
+        assert_eq!(crate::day02::part1(inp.trim()).unwrap(), 3409710);
+        assert_eq!(crate::day02::part2(inp.trim()).unwrap(), 7912);
+    }
+
+    #[test]
+    fn day03() {
+        let inp = include_str!("../input/day03");
+        assert_eq!(crate::day03::part1(inp.trim()).unwrap(), 855);
+        assert_eq!(crate::day03::part2(inp.trim()).unwrap(), 11238);
+    }
+
+    #[test]
+    fn day04() {
+        let inp = include_str!("../input/day04");
+        assert_eq!(crate::day04::part1(inp.trim()).unwrap(), 921);
+        assert_eq!(crate::day04::part2(inp.trim()).unwrap(), 603);
+    }
+
+    #[test]
+    fn day05() {
+        let inp = include_str!("../input/day05");
+        assert_eq!(crate::day05::part1(inp.trim()).unwrap(), 9431221);
+        assert_eq!(crate::day05::part2(inp.trim()).unwrap(), 1409363);
+    }
+
+    #[test]
+    fn day06() {
+        let inp = include_str!("../input/day06");
+        assert_eq!(crate::day06::part1(inp.trim()).unwrap(), 249308);
+        assert_eq!(crate::day06::part2(inp.trim()).unwrap(), 349);
+    }
+
+    #[test]
+    fn day07() {
+        let inp = include_str!("../input/day07");
+        assert_eq!(crate::day07::part1(inp.trim()).unwrap(), 34852);
+        assert_eq!(crate::day07::part2(inp.trim()).unwrap(), 44282086);
+    }
+
+    #[test]
+    fn day08() {
+        let inp = include_str!("../input/day08");
+        let mut p2 = String::new();
+        p2.push('\n');
+        p2.push_str("  ##  ##  ###   ##  ###  \n");
+        p2.push_str("   # #  # #  # #  # #  # \n");
+        p2.push_str("   # #    #  # #    ###  \n");
+        p2.push_str("   # #    ###  #    #  # \n");
+        p2.push_str("#  # #  # # #  #  # #  # \n");
+        p2.push_str(" ##   ##  #  #  ##  ###  \n");
+        assert_eq!(crate::day08::part1(inp.trim()).unwrap(), 1474);
+        assert_eq!(crate::day08::part2(inp.trim()).unwrap(), p2);
+    }
+
+    #[test]
+    fn day09() {
+        let inp = include_str!("../input/day09");
+        assert_eq!(crate::day09::part1(inp.trim()).unwrap(), 3780860499);
+        assert_eq!(crate::day09::part2(inp.trim()).unwrap(), 33343);
+    }
+
+    #[test]
+    fn day10() {
+        let inp = include_str!("../input/day10");
+        assert_eq!(crate::day10::part1(inp.trim()).unwrap(), 230);
+        assert_eq!(crate::day10::part2(inp.trim()).unwrap(), 1205);
+    }
+
+    #[test]
+    fn day11() {
+        let inp = include_str!("../input/day11");
+        let mut p2 = String::new();
+        p2.push('\n');
+        p2.push_str(" ###   ##  #  # ###  #    ###  #  # #  #   \n");
+        p2.push_str(" #  # #  # # #  #  # #    #  # #  # # #    \n");
+        p2.push_str(" #  # #    ##   #  # #    #  # #  # ##     \n");
+        p2.push_str(" ###  #    # #  ###  #    ###  #  # # #    \n");
+        p2.push_str(" #    #  # # #  # #  #    #    #  # # #    \n");
+        p2.push_str(" #     ##  #  # #  # #### #     ##  #  #   \n");
+        assert_eq!(crate::day11::part1(inp.trim()).unwrap(), 2373);
+        assert_eq!(crate::day11::part2(inp.trim()).unwrap(), p2);
+    }
+
+    #[test]
+    fn day12() {
+        let inp = include_str!("../input/day12");
+        assert_eq!(crate::day12::part1(inp.trim()).unwrap(), 10055);
+        assert_eq!(crate::day12::part2(inp.trim()).unwrap(), 374307970285176);
+    }
+
+    #[test]
+    fn day13() {
+        let inp = include_str!("../input/day13");
+        assert_eq!(crate::day13::part1(inp.trim()).unwrap(), 200);
+        assert_eq!(crate::day13::part2(inp.trim()).unwrap(), 9803);
+    }
+
+    #[test]
+    fn day14() {
+        let inp = include_str!("../input/day14");
+        assert_eq!(crate::day14::part1(inp.trim()).unwrap(), 278404);
+        assert_eq!(crate::day14::part2(inp.trim()).unwrap(), 4436981);
+    }
+
+    #[test]
+    fn day15() {
+        let inp = include_str!("../input/day15");
+        assert_eq!(crate::day15::part1(inp.trim()).unwrap(), 366);
+        assert_eq!(crate::day15::part2(inp.trim()).unwrap(), 384);
+    }
+
+    #[test]
+    fn day16() {
+        let inp = include_str!("../input/day16");
+        assert_eq!(crate::day16::part1(inp.trim()).unwrap(), 69549155);
+        assert_eq!(crate::day16::part2(inp.trim()).unwrap(), 83253465);
+    }
+
+    #[test]
+    fn day17() {
+        let inp = include_str!("../input/day17");
+        assert_eq!(crate::day17::part1(inp.trim()).unwrap(), 3888);
+        assert_eq!(crate::day17::part2(inp.trim()).unwrap(), 927809);
+    }
+
+    #[test]
+    fn day18() {
+        let inp = include_str!("../input/day18");
+        assert_eq!(crate::day18::part1(inp.trim()).unwrap(), 4954);
+        assert_eq!(crate::day18::part2(inp.trim()).unwrap(), 2334);
+    }
+
+    #[test]
+    fn day19() {
+        let inp = include_str!("../input/day19");
+        assert_eq!(crate::day19::part1(inp.trim()).unwrap(), 110);
+        assert_eq!(crate::day19::part2(inp.trim()).unwrap(), 17302065);
+    }
 }
